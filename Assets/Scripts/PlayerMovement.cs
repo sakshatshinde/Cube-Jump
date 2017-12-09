f5e2c270abe3dc6b0470c2c080879b5c7ae71a15
Sakshat Shinde <shindesakshat@gmail.com>
2017-12-09 22:40:37 +0530

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+public class PlayerMovement : MonoBehaviour {
+	public Rigidbody rb;
+	public float forwardForce = 2000f;
+	public float directionalForce = 500;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		rb.AddForce (0, 0, forwardForce * Time.deltaTime);
+
+		if( Input.GetKey("d"))
+		{
+			rb.AddForce (directionalForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if( Input.GetKey("a"))
+		{
+			rb.AddForce (-directionalForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if( Input.GetKey("w"))
+		{
+			rb.AddForce (0, 0, directionalForce * Time.deltaTime, ForceMode.VelocityChange);
+		}
+
+		if (rb.position.y < -1f) {
+			FindObjectOfType<GameManager>().EndGame();
+		}
+
+}
+}
